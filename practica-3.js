980baa86fd1106edda7e9f47fb98cd90334c0559
Carlos <karlosssanta@gmail.com>
2020-02-14 20:24:37 +0100

    add comments

--- practica-3.js
+++ practica-3.js
@@ -7,23 +7,32 @@ for (i = 0; i < divAnnos.length; i++){
     divAnnos.eq(i).attr("class", divAnnos.eq(i).attr("id"));
 }
 
+//Seleccionamos el elemento con el id 'heroes'
 var heroes = $("#heroes");
+//Seleccionamos todos sus padres hasta el primer 'div'.
+//Así almacenamos la década y el género
 var padresHeroes = heroes.parentsUntil("#container");
+//en la primera posición el género (al no tener id (y no poder ponérselo porque no es único) seleccionamos la clase)
 var generoHeroes = padresHeroes.eq(0).attr("class")
+//y en la segunda la década
 var decadaHeroes = padresHeroes.eq(1).attr("id")
 alert("Héroes del silencio es de la Década: " + decadaHeroes + " y del Género: " + generoHeroes);
 
 var siguienteHijo = heroes;
 while (siguienteHijo.next().length != 0) {
+    //Seleccionamos el sigiente hijo
     siguienteHijo = siguienteHijo.next();
+    //Y le ponemos como atributo el mismo contenido de la etiqueta (en minúsculas por convención)
     siguienteHijo.attr("id", siguienteHijo.text().toLowerCase());
 }
  
-var queen = $(".internacional").find("queen")
-
+//Comprobamos si existe un artista con el id queen
+var queen = $(".internacional").find("#queen")
+//si no existe
 if (queen.length === 0) {
+    //Creamos el artista Queen con el id queen
     queen = $("<p id=\"queen\">Queen</p>")
+    //Y lo añadimos a la década de los ochenta en el género internacional
     var internacionalOchenta = $("#ochenta .internacional")
     internacionalOchenta.append(queen)
 }
-console.log(internacionalOchenta)
